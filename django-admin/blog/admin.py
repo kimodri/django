cbffb437c92e3f633128b3e0b89c0e252e58b596
Kim Audrey Magan <work.kimmagan@gmail.com>
2025-12-09 19:10:09 +0800

    Added new notes

--- django-admin/blog/admin.py
+++ django-admin/blog/admin.py
@@ -38,12 +38,12 @@ class postAdmin(admin.ModelAdmin):
     # Here is where you connect a form
     form = forms.PostAdminForm
 
-# # You are writing a new admin area here
-# class BlogAdminArea(admin.AdminSite):
-#     site_header = "Blog Admin Area"
-#     site_title = "Blogs"
+# You are writing a new admin area here
+class BlogAdminArea(admin.AdminSite):
+    site_header = "Blog Admin Area"
+    site_title = "Blogs"
 
-# blog_site = BlogAdminArea(name="BlogAdmin")
+blog_site = BlogAdminArea(name="BlogAdmin")
 
-# blog_site.register(models.Post)
-# blog_site.register(models.Category)
+blog_site.register(models.Post)
+blog_site.register(models.Category)
